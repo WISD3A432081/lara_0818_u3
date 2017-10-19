5c16d92ce581e65e23c5bc50064f04520f1d3824
chan\admin <s707071127@gmail.com>
2017-10-19 16:48:21 +0800

    3A432081陳紹田-執行migration,adminer介面內確認變更´

--- database/migrations/2017_10_19_083927_add_is_feature_in_posts_table.php
+++ database/migrations/2017_10_19_083927_add_is_feature_in_posts_table.php
@@ -14,8 +14,7 @@ class AddIsFeatureInPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->string('title',500)->change();
-            $table->boolean('is_feature')->default(false)->after('content');
+
         });
     }
 
@@ -27,7 +26,8 @@ class AddIsFeatureInPostsTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->string('title',500)->change();
+            $table->boolean('is_feature')->default(false)->after('content');
         });
     }
 }
